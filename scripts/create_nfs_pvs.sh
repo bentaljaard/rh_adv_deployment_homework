40d84fa7c2c665fc62302a7d70f337c92d62b86e
Ben Taljaard <ben.taljaard@devoteam.com>
2018-11-13 20:04:15 +0100

    rework scripts and update playbook

--- scripts/create_nfs_pvs.sh
+++ scripts/create_nfs_pvs.sh
@@ -4,6 +4,9 @@ mkdir -p /srv/nfs/user-vols/pv{1..200}
 
 echo "Creating PV for users.."
 
+#Empty out the exports file so that this script can be run multiple times
+echo "# Openshift User Volumes" > /etc/exports.d/openshift-uservols.exports
+
 for pvnum in {1..50} ; do
   echo '/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)' >> /etc/exports.d/openshift-uservols.exports
   chown -R nfsnobody.nfsnobody /srv/nfs
