5270e5f5959e3a27c0a93a2e77f869fd526d3f67
root <root@bastion.537f.example.opentlc.com>
2018-11-13 08:40:39 +0000

    reload inventory before running included playbook

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Copy a new hosts file to /etc/ansible/hosts"
-cp /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
+cp /root/rh_adv_deployment_homework/ocp_inventory /etc/ansible/hosts
 
 echo "Set the current GUID to generate the inventory"
-GUID=`hostname|awk -F. '{print $2}'`
+#GUID=`hostname|awk -F. '{print $2}'`
+GUID=$1
 sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
