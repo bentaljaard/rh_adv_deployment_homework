bbe571d51a0cd627cf672ed03136ca6888db8eba
root <root@bastion.537f.example.opentlc.com>
2018-11-13 08:41:47 +0000

    remove unused script

--- scripts/generate_inventory.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Copy a new hosts file to /etc/ansible/hosts"
-cp /root/rh_adv_deployment_homework/ocp_inventory /etc/ansible/hosts
-
-echo "Set the current GUID to generate the inventory"
-#GUID=`hostname|awk -F. '{print $2}'`
-GUID=$1
-sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
