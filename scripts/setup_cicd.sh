7e1b52980959c1f3137db9a34c66861a1540b0d8
Ben Taljaard <ben.taljaard@devoteam.com>
2018-11-20 13:21:14 +0100

    add new jenkins template

--- scripts/setup_cicd.sh
+++ scripts/setup_cicd.sh
@@ -10,16 +10,15 @@ fi
 # Check if jenkins is deployed
 if (( $(oc get dc -n cicd-dev|grep jenkins|wc -l) == 0 )); then
 	# Jenkins is not deployed yet
-	oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n cicd-dev
-	oc rollout pause dc/jenkins -n cicd-dev
-	oc set resources dc/jenkins --limits=cpu=2 --requests=cpu=1 -n cicd-dev
-	oc rollout resume dc/jenkins -n cicd-dev
+	oc new-app -f /root/rh_adv_deployment_homework/resources/jenkins_template.yaml \
+	-p MEM_REQUESTS=1Gi -p MEM_LIMITS=2Gi -p VOLUME_CAPACITY=4G \
+	-p CPU_REQUESTS=1000m -p CPU_LIMITS=1500m -n cicd-dev
 fi
 
 # Check if Jenkins is up
 while : ; do
   echo "Checking if Jenkins is Ready..."
-  oc get pod -n ${GUID}-jenkins|grep -v deploy|grep -v build|grep "1/1"
+  oc get pod -n cicd-dev|grep -v deploy|grep -v build|grep "1/1"
   [[ "$?" == "1" ]] || break
   echo "...no. Sleeping 10 seconds."
   sleep 10
