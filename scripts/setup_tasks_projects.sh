59d158e2d55ab3e6fe6047b246ba92da3790948b
Ben Taljaard <ben.taljaard@devoteam.com>
2018-11-22 13:16:01 +0100

    add ci setup bits

--- scripts/setup_tasks_projects.sh
+++ scripts/setup_tasks_projects.sh
@@ -23,7 +23,7 @@ oc policy add-role-to-group system:image-puller system:serviceaccounts:tasks-dev
 
 # Setup objects in projects
 if (( $(oc get dc -n tasks-dev|wc -l) == 0 )); then
-	oc new-app tasks-build/tasks:0.0-0 --name=tasks-dev --allow-missing-imagestream-tags=true --allow-missing-images -n tasks-dev
+	oc new-app tasks-build/tasks:0.0-0 --name=tasks-dev --allow-missing-imagestream-tags=true --allow-missing-images=true -n tasks-dev
 	oc set triggers dc/tasks-dev --remove-all -n tasks-dev
 	oc set resources dc/tasks-dev --limits=cpu=250m,memory=512Mi --requests=cpu=100m,memory=300Mi -n tasks-dev
 	oc set probe dc/tasks-dev -n tasks-dev --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
@@ -33,7 +33,7 @@ if (( $(oc get dc -n tasks-dev|wc -l) == 0 )); then
 fi
 
 if (( $(oc get dc -n tasks-test|wc -l) == 0 )); then
-	oc new-app tasks-dev/tasks:0.0-0 --name=tasks-test --allow-missing-imagestream-tags=true --allow-missing-images -n tasks-test
+	oc new-app tasks-dev/tasks:0.0-0 --name=tasks-test --allow-missing-imagestream-tags=true --allow-missing-images=true -n tasks-test
 	oc set triggers dc/tasks-test --remove-all -n tasks-test
 	oc set resources dc/tasks-test --limits=cpu=250m,memory=512Mi --requests=cpu=100m,memory=300Mi -n tasks-test
 	oc set probe dc/tasks-test -n tasks-test --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
@@ -43,7 +43,7 @@ if (( $(oc get dc -n tasks-test|wc -l) == 0 )); then
 fi
 
 if (( $(oc get dc -n tasks-prod|wc -l) == 0 )); then
-	oc new-app tasks-test/tasks:0.0-0 --name=tasks-prod --allow-missing-imagestream-tags=true --allow-missing-images -n tasks-prod
+	oc new-app tasks-test/tasks:0.0-0 --name=tasks-prod --allow-missing-imagestream-tags=true --allow-missing-images=true -n tasks-prod
 	oc set triggers dc/tasks-prod --remove-all -n tasks-prod
 	oc set resources dc/tasks-prod --limits=cpu=250m,memory=512Mi --requests=cpu=100m,memory=300Mi -n tasks-prod
 	oc set probe dc/tasks-prod -n tasks-prod --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
